375cc944a54f5101e262438074aad349b0ef297b
pawelkom88 <pawel.komorkiewicz@yahoo.co.uk>
2025-04-03 21:32:33 +0100

    add login method to user controller

--- backend/controllers/user-controller.ts
+++ backend/controllers/user-controller.ts
@@ -1,6 +1,7 @@
 import {Request, Response} from "express";
 import {User} from "../models/user.model";
 import {hashPassword} from "./helpers/hashPassword.ts";
+import {comparePasswords} from "./helpers/comparePasswords.ts";
 
 export class UserController {
     async register(request: Request, response: Response) {
@@ -16,7 +17,7 @@ export class UserController {
                 });
             }
 
-            const hashedPassword  = await hashPassword(request.body.password);
+            const hashedPassword = await hashPassword(request.body.password);
 
             const newUser = new User({
                 ...request.body,
@@ -26,12 +27,50 @@ export class UserController {
             await newUser.save();
 
             return response.status(201).send(newUser);
-        } catch (error) {
-            console.error(error);
-            return response.status(500).json({
-                error: true,
-                message: "Cannot register a user",
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                return response.status(500).json({
+                    error: true,
+                    message: "Cannot register a user",
+                });
+            }
+        }
+    }
+
+    async login(request: Request, response: Response) {
+        try {
+            const existingUser = await User.findOne({
+                username: request.body.username,
             });
+
+            if (!existingUser) {
+                return response.status(400).json({
+                    error: true,
+                    message: "Username or password is incorrect",
+                });
+            }
+
+            const isPasswordValid = await comparePasswords(request.body.password, existingUser.password);
+
+            if (!isPasswordValid) {
+                return response.status(400).json({
+                    error: true,
+                    message: "Username or password is incorrect",
+                });
+            }
+
+            return response.status(200).send({
+                success: true,
+                message: "User logged in successfully",
+            });
+
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                return response.status(500).json({
+                    error: true,
+                    message: "Cannot login. Please try again later.",
+                });
+            }
         }
     }
 }
--- backend/db/config.ts
+++ backend/db/config.ts
@@ -1,7 +1,10 @@
-
 import mongoose from "mongoose";
 
 export default function connectDB() {
+    if (!process.env.DB_URI) {
+        throw new Error("uri is required");
+    }
+
     mongoose
         .connect(process.env.DB_URI)
         .then(() => {
