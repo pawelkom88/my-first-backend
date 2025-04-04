07006babfeaadd33f0c08f6fb5e8ea4b8e0a5c3e
pawelkom88 <pawel.komorkiewicz@yahoo.co.uk>
2025-04-04 16:46:40 +0100

    refactor

--- backend/routes/usersRouter.ts
+++ backend/routes/usersRouter.ts
@@ -3,18 +3,21 @@ import {routes} from "./routes";
 import {UserController} from "../controllers/user-controller";
 import type {AuthenticatedRequest} from "../middleware/security.ts";
 import { authMiddleware} from "../middleware/security.ts";
+
 export const usersRouter = express.Router();
 
 const user = new UserController();
 
 usersRouter.post(`/${routes.register}`, user.register as RequestHandler);
 usersRouter.post(`/${routes.login}`, user.login as RequestHandler);
+// @ts-expect-error - hard to type
 usersRouter.post(`/${routes.logout}`, authMiddleware, user.logout);
+// @ts-expect-error - hard to type
 usersRouter.get(`/${routes.userDetails}`, authMiddleware, (request: AuthenticatedRequest, response: Response) => {
     if (!request.user) {
         return response.status(401).json({
             error: true,
-            message: "???"
+            message: "Could not retrieve user details"
         });
     }
     response.status(200).json({ user: request.user });
